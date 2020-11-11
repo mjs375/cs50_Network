836db180cc29886bf7a8a389482d4c440d0f4b69
mjs375 <spitzer.matthewjames@gmail.com>
2020-11-11 18:32:58 -0500

    Added test.py & ci.yml testing

--- project4/network/tests.py
+++ project4/network/tests.py
@@ -1,25 +1,60 @@
 from django.test import Client, TestCase # TestCase: will create new DB for testing purposes only
-# from .models import #class, class, class
+from .models import User, Post
 
 
+"""
+Run tests manually:
+    $ python3 manage.py test
+Run ON PUSH:
+    1. Create .yml file
+    2. $ git add/commit/push
+    3. Check Github.com
+"""
 
 # # # Create your tests here.
 
 
+class LogInTest(TestCase): # TEST LOGIN PROCESS
+    def setUp(self): # create a dummy user
+        self.credentials = {
+        'username': 'testuser',
+        'password': 'dummypassword',
+        }
+        User.objects.create_user(**self.credentials)
+    def test_login(self): # try to login dummy user
+        # send login data:
+        response = self.client.post('/login', self.credentials, follow=True)
+        # should be logged in now:
+        self.assertTrue(response.context['user'].is_authenticated)
+
+
+"""
 class NetworkTestCase(TestCase):
     #
     def setUp(self): # create dummy data for tests
         # Create users
-        #
+        chip = User.objects.create(username="Chip", email="chip@munk.com", password="chipper")
+        # dale = User.objects.create(username)
         # Create tweets
         #
         pass
 
     #
-    def test_one(self):
+    def test_post_squeak(self):
         #
-        # self.assert____(...)
+        self.assertEqual()
         pass
-    #
+
+
+
     def test_two(self):
         pass
+"""
+
+
+"""
+Django uses Python's UNITTEST, a class-based approach to testing.
+
+
+
+"""
